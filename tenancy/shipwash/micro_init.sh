7c79c8280db944b0c8fd41c3ec69cfeafd7979b9
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2022-12-05 17:10:08 -0500

    Update micro_init.sh

--- tenancy/shipwash/micro_init.sh
+++ tenancy/shipwash/micro_init.sh
@@ -20,13 +20,14 @@ servicename=customboot
 cat > $bootscript <<-'EOF'
 	#!/usr/bin/env bash
 	echo "$bootscript ran at \$(date)!" > /tmp/it-works
-  docker run -d --restart=always --name restreamer \
-    -v /opt/restreamer/config:/core/config \
-    -v /opt/restreamer/data:/core/data \
-    -p 8080:8080 -p 8181:8181 \
-    -p 1935:1935 -p 1936:1936 \
-    -p 6000:6000/udp \
-    datarhei/restreamer:latest
+	# https://docs.datarhei.com/restreamer/getting-started/quick-start
+	docker run -d --restart=always --name restreamer \
+		-v /opt/restreamer/config:/core/config \
+		-v /opt/restreamer/data:/core/data \
+		-p 8080:8080 -p 8181:8181 \
+		-p 1935:1935 -p 1936:1936 \
+		-p 6000:6000/udp \
+		datarhei/restreamer:latest
 EOF
 
 chmod +x $bootscript
