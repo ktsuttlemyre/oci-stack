6567a3b5992ebd49accd2e349b9c2dcfcd341cb2
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2022-12-05 17:08:02 -0500

    Update micro_init.sh

--- tenancy/shipwash/micro_init.sh
+++ tenancy/shipwash/micro_init.sh
@@ -1,2 +1,42 @@
 #!/bin/bash -xe
 echo "Hello, this is a micro init script. If you are seeing this then the init script has worked!" > ./init_success
+
+
+sudo apt-get update
+#sudo apt-get upgrade
+
+#https://www.docker.com/blog/getting-started-with-docker-for-arm-on-linux/
+curl -fsSL test.docker.com -o get-docker.sh && sh get-docker.sh
+
+#Add the current user to the docker group to avoid needing sudo to run the docker command:
+sudo usermod -aG docker $USER 
+
+
+####################
+#  Create Service  #
+bootscript=/root/boot.sh
+servicename=customboot
+
+cat > $bootscript <<-'EOF'
+	#!/usr/bin/env bash
+	echo "$bootscript ran at \$(date)!" > /tmp/it-works
+  docker run -d --restart=always --name restreamer \
+    -v /opt/restreamer/config:/core/config \
+    -v /opt/restreamer/data:/core/data \
+    -p 8080:8080 -p 8181:8181 \
+    -p 1935:1935 -p 1936:1936 \
+    -p 6000:6000/udp \
+    datarhei/restreamer:latest
+EOF
+
+chmod +x $bootscript
+
+cat > /etc/systemd/system/$servicename.service <<-'EOF'
+	[Service]
+	ExecStart=$bootscript
+	[Install]
+	WantedBy=default.target
+	EOF
+
+systemctl enable $servicename
+
