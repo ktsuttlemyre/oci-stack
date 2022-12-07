584bdb93f0904f3925ae5f67cbb1a345790c4a2d
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2022-12-07 12:43:47 -0500

    Update 1_mini-1.sh

--- tenancy/killerqueensouthflorida/1_mini-1.sh
+++ tenancy/killerqueensouthflorida/1_mini-1.sh
@@ -1,14 +1,8 @@
-sudo -i
-echo "Changed to user whoami: $(whoami) username: $USERNAME"
+
 
 ####################
 #  Create Service  #
-bootscript=/root/boot.sh
-servicename=customboot
-
-cat > $bootscript <<-'EOF'
-	#!/usr/bin/env bash
-	echo "$bootscript ran at \$(date)!" > /tmp/it-works
+BOOTSCRIPT=$(cat <<-END
 	# https://docs.datarhei.com/restreamer/getting-started/quick-start
 	docker run -d --restart=always --name restreamer \
 		-v /opt/restreamer/config:/core/config \
@@ -17,19 +11,5 @@ cat > $bootscript <<-'EOF'
 		-p 1935:1935 -p 1936:1936 \
 		-p 6000:6000/udp \
 		datarhei/restreamer:latest
-EOF
-
-chmod +x $bootscript
-
-cat > /etc/systemd/system/$servicename.service <<-'EOF'
-	[Service]
-	ExecStart=$bootscript
-	[Install]
-	WantedBy=default.target
-	EOF
-
-systemctl enable $servicename
-
-
-echo "Hello, this is a $(hostname) init script. If you are seeing this then the init script has finished!" | tee >> /init_start
-sudo shutodwn -r now
+END
+)
