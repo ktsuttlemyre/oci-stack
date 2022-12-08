fed1688f3a7aa009cd87cff8a38145e7d890f487
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2022-12-07 20:04:17 -0500

    Update head_ampere_mini-1_mini-2.sh

--- tenancy/killerqueensouthflorida/head_ampere_mini-1_mini-2.sh
+++ tenancy/killerqueensouthflorida/head_ampere_mini-1_mini-2.sh
@@ -1,13 +1,16 @@
 #!/bin/bash -xe
 echo "Hello, this is a $(hostname) init script. If you are seeing this then the init script has started!" | tee > /init.log
 
-#got is a way of checking out read only versions of git 
+#Slim git clone/pull read only function 
 got () {
 	# 1= user 2 = repo
 	BRANCH=${3:-master}
 	[ -d "$2" ] && rm -rf "$2"
 	curl -L -O "https://github.com/$1/$2/archive/$BRANCH.zip" | gunzip -S
 }
+
+# set a variable permenantly 
+let(){ declare -xg $1=\"$2\" ; echo \"export $1='$2'\" >> /etc/environment ; }
 
 echo "whoami $(whoami) username: $USERNAME"
 #update and install tools
@@ -25,14 +28,17 @@ chmod +x /tmp/oci_install.sh
 /tmp/oci_install.sh --accept-all-defaults
 
 #https://database-heartbeat.com/2021/10/05/auth-cli/
-export OCI_CLI_AUTH=instance_principal
-#make perminant
-echo "export OCI_CLI_AUTH=instance_principal" > /etc/environment
+#set permanant
+let OCI_CLI_AUTH 'instance_principal'
+
+
+SECRET() {
+	oci secrets secret-bundle get-secret-bundle-by-name --vault-id $VAULT --secret-name "$1" | jq -r '.data."secret-bundle-content".content | @base64d'
+}
 
 #get secret
-oci secrets secret-bundle get-secret-bundle-by-name --vault-id "ocid1.vault.oc1.iad.b5ry7zhyaaaes.abuw" 
-CLOUDFLARE_TOKEN=""
-CLOUDFLARE_ZONEID=""
+CLOUDFLARE_TOKEN=SECRET CLOUDFLARE_TOKEN
+CLOUDFLARE_ZONEID=SECRET CLOUDFLARE_ZONEID
 
 
 #https://github.com/timothymiller/cloudflare-ddns
