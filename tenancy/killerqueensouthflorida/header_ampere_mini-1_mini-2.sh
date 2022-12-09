4519b2e0b01443969719980ce886b50e47801ca0
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2022-12-09 17:01:48 -0500

    Update header_ampere_mini-1_mini-2.sh

--- tenancy/killerqueensouthflorida/header_ampere_mini-1_mini-2.sh
+++ tenancy/killerqueensouthflorida/header_ampere_mini-1_mini-2.sh
@@ -25,7 +25,7 @@ chmod +x /run/oci/oci_install.sh
 #let OCI_CLI_AUTH 'instance_principal'
 EOF
 
-#purposely end and restart the session here
+#purposely end and restart the ubuntu user session here
 #refresh shell so we can use oci cli
 ##############################
 ####     USER  UBUNTU     ####
@@ -73,3 +73,44 @@ curl -fsSL test.docker.com -o get-docker.sh && sh get-docker.sh
 #Add ubuntu to the docker group to avoid needing sudo to run the docker command:
 sudo usermod -aG docker $USERNAME 
 EOF
+##############################
+####     USER  ROOT     ####
+##############################
+
+####################
+#  Create Service  #
+BOOTSCRIPT=$(cat <<-END
+	# https://docs.datarhei.com/restreamer/getting-started/quick-start
+	docker run -d --restart=always --name restreamer \
+		-v /opt/restreamer/config:/core/config \
+		-v /opt/restreamer/data:/core/data \
+		-p 8080:8080 -p 8181:8181 \
+		-p 1935:1935 -p 1936:1936 \
+		-p 6000:6000/udp \
+		datarhei/restreamer:latest
+END
+)
+
+#change back to root and set everything else up
+BOOTSCRIPT_PATH=/root/boot.sh
+servicename=customboot
+
+cat > $BOOTSCRIPT_PATH <<-'EOF'
+	#!/usr/bin/env bash
+	echo "$BOOTSCRIPT_PATH ran at \$(date)!" > /tmp/it-works
+	"$BOOTSCRIPT"
+EOF
+
+chmod +x $bootscript
+
+cat > /etc/systemd/system/$servicename.service <<-'EOF'
+	[Service]
+	ExecStart=$bootscript
+	[Install]
+	WantedBy=default.target
+	EOF
+
+systemctl enable $servicename
+
+echo "Hello, this is a $(hostname) init script. If you are seeing this then the init script has finished! Restarting now" | tee >> /init_start
+sudo shutodwn -r now
