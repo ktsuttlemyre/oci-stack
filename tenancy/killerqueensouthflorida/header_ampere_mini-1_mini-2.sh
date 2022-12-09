0f5484a90baaeafbf6639ca6a0646187be663f70
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2022-12-09 16:42:01 -0500

    Update header_ampere_mini-1_mini-2.sh

--- tenancy/killerqueensouthflorida/header_ampere_mini-1_mini-2.sh
+++ tenancy/killerqueensouthflorida/header_ampere_mini-1_mini-2.sh
@@ -1,48 +1,18 @@
 #!/bin/bash -xe
-#cloud_init
+#cloud_init docs
 #https://cloudinit.readthedocs.io/en/latest/topics/examples.html
 echo "Hello, this is a $(hostname) init script. If you are seeing this then the init script has started!" | tee > /init.log
-
-#Slim git clone/pull read only function 
-got () {
-	# 1= user 2 = repo
-	BRANCH=${3:-master}
-	[ -d "$2" ] && rm -rf "$2"
-	curl -L -O "https://github.com/$1/$2/archive/$BRANCH.zip" | gunzip -S
-}
-
-# set a variable permenantly 
-let(){
-	local source
-	if [[ $(type -t "$1") == function ]];
-		source=$(type SECRET | tail -n +2)
-	then
-		declare -xg $1="$2"
-		source = "export $1='$2'"
-	fi
-	echo "$source" | tee -a /root/.profile /home/ubuntu/.profile
-}
-
-change_user () {
-	sudo -i -u ubuntu
-	echo "Changed user [whoami: $(whoami)] [USERNAME=$USERNAME] [SUDO_USER=$SUDO_USER]"
-}
-
-SECRET() {
-	oci secrets secret-bundle get-secret-bundle-by-name --vault-id $VAULT --secret-name "$1" | jq -r '.data."secret-bundle-content".content | @base64d'
-}
-
-let change_user
-let let
-let got
-let secret
 
 #sudo user
 #disable ubuntu firewall
 command -v ufw > /dev/null && ufw disable
 
-#go to admin user
-change_user "ubuntu"
+
+#go to admin user ubuntu
+##############################
+####     USER  UBUNTU     ####
+##############################
+sudo -i -u ubuntu bash << EOF
 
 # install oci cli
 curl -L -o /run/oci/oci_install.sh https://raw.githubusercontent.com/oracle/oci-cli/master/scripts/install/install.sh
@@ -53,12 +23,16 @@ chmod +x /run/oci/oci_install.sh
 #depricated this was when we used policies
 #set permanant
 #let OCI_CLI_AUTH 'instance_principal'
+EOF
 
+#purposely end and restart the session here
 #refresh shell so we can use oci cli
-exec -l $SHELL
-
+##############################
+####     USER  UBUNTU     ####
+##############################
+sudo -i -u ubuntu bash << EOF
 #write config to disk from envoronment variable
-echo “$OCI_CONFIG” | base64 -d | tar -xz
+echo "$OCI_CONFIG" | base64 -d | tar -xz
 oci setup repair-file-permissions –file ~/.oci/oci_api.pem
 oci setup repair-file-permissions –file ~/.oci/config
 
@@ -69,7 +43,7 @@ CLOUDFLARE_ZONEID=SECRET CLOUDFLARE_ZONEID
 #install DDNS for cloudflare
 #https://github.com/timothymiller/cloudflare-ddns
 got "timothymiller" "cloudflare-ddns"
-cat > cloudflare-ddns/config.json <<-'EOF'
+cat > cloudflare-ddns/config.json <<-'EOF1'
 	{
 	  "cloudflare": [
 	    {
@@ -90,7 +64,7 @@ cat > cloudflare-ddns/config.json <<-'EOF'
 	  "purgeUnknownRecords": false,
 	  "ttl": 300
 	}
-	EOF
+	EOF1
 
 
 
