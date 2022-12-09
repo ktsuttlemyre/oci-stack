47f981e8d340bb40c01bf84fa8f436a5e7912f60
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2022-12-09 11:55:09 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -150,9 +150,9 @@ resource "oci_core_instance" "this" {
     ssh_authorized_keys = var.ssh_public_key
     user_data = base64encode(join("\n",concat([
         "#!/bin/bash -ex",
-        "let(){ declare -xg $1=\"$2\" ; echo \"export $1='$2'\" >> /etc/environment ; }",
+        "let(){ declare -xg $1=\"$2\" ; echo \"export $1='$2'\" | tee -a /home/ubuntu/.profile /root/.profile ; }",
         "let VAULT ${data.oci_kms_vaults.this.vaults[index(data.oci_kms_vaults.this.vaults.*.display_name, data.oci_identity_tenancy.tenancy.name)].id}",
-	"OCI_CONFIG=${data.oci_secrets_secretbundle.this.secret_bundle_content[0].content}"],
+	"let OCI_CONFIG ${data.oci_secrets_secretbundle.this.secret_bundle_content[0].content}"],
 	    [for fn in fileset(".", "./tenancy/${data.oci_identity_tenancy.tenancy.name}/**mini-${count.index + 1}**") : file(fn)]
 	)))
   }
@@ -207,9 +207,9 @@ resource "oci_core_instance" "that" {
     ssh_authorized_keys = var.ssh_public_key
     user_data = base64encode(join("\n",concat([
         "#!/bin/bash -ex",
-        "let(){ declare -xg $1=\"$2\" ; echo \"export $1='$2'\" >> /etc/environment ; }",
+        "let(){ declare -xg $1=\"$2\" ; echo \"export $1='$2'\" | tee -a /home/ubuntu/.profile /root/.profile ; }",
         "let VAULT ${data.oci_kms_vaults.this.vaults[index(data.oci_kms_vaults.this.vaults.*.display_name, data.oci_identity_tenancy.tenancy.name)].id}",
-	"OCI_CONFIG=${data.oci_secrets_secretbundle.this.secret_bundle_content[0].content}"],
+	"let OCI_CONFIG ${data.oci_secrets_secretbundle.this.secret_bundle_content[0].content}"],
 	[for fn in fileset(".", "./tenancy/${data.oci_identity_tenancy.tenancy.name}/**ampere**") : file(fn)]
 	)))
   }
