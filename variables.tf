45cfd221f89351859797d3b70b3a86fceb1934e5
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2022-12-11 16:20:33 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,7 +6,7 @@ variable "tenancy_ocid" {
 variable "name" {
   description = "Display name for resources"
   type        = string
-  default     = ""
+  default     = "rogue-stack"
 }
 
 variable "cidr_block" {
