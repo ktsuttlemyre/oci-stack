d991a4d0acbde188f6b69c09685a7642f604c5d5
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2022-12-11 13:01:20 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -176,7 +176,7 @@ runcmd:
   - "var(){ export $1=\"$2\" ; echo \"$1='$2'\" | tee -a /home/ubuntu/.profile /root/.profile ; }"
   - "var VAULT ${data.oci_kms_vaults.this.vaults[index(data.oci_kms_vaults.this.vaults.*.display_name, data.oci_identity_tenancy.tenancy.name)].id}"
   - "var OCI_CONFIG ${data.oci_secrets_secretbundle.this.secret_bundle_content[0].content}"
-  - "[ \"$EUID\" -ne 0 ] && echo \"Run this script as root\" && exit"
+  - "[[ $EUID > 0 ]] && echo \"Run this script as root\" && exit"
   - "cd $HOME"
   - "$HOME/init_script.sh"
 write_files:
@@ -251,7 +251,7 @@ runcmd:
   - "var(){ export $1=\"$2\" ; echo \"$1='$2'\" | tee -a /home/ubuntu/.profile /root/.profile ; }"
   - "var VAULT ${data.oci_kms_vaults.this.vaults[index(data.oci_kms_vaults.this.vaults.*.display_name, data.oci_identity_tenancy.tenancy.name)].id}"
   - "var OCI_CONFIG ${data.oci_secrets_secretbundle.this.secret_bundle_content[0].content}"
-  - "[ \"$EUID\" -ne 0 ] && echo \"Run this script as root\" && exit"
+  - "[[ $EUID > 0 ]] && echo \"Run this script as root\" && exit"
   - "cd $HOME"
   - "$HOME/init_script.sh"
 write_files:
