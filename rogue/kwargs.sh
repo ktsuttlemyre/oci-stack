e63d408bf0559e0d10848ca5bdb0f14b1d8af7ba
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2022-12-12 17:42:49 -0500

    Update kwargs.sh

--- rogue/kwargs.sh
+++ rogue/kwargs.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+# Resources
+# https://bl.ocks.org/magnetikonline/22c1eb412daa350eeceee76c97519da8
+# https://stackoverflow.com/questions/4069188/how-to-pass-an-associative-array-as-argument-to-a-function-in-bash
+# https://stackoverflow.com/questions/3966048/access-arguments-to-bash-script-inside-a-function
+# https://stackoverflow.com/questions/35235707/bash-how-to-avoid-command-eval-set-evaluating-variables
+# jq read json to associative array https://stackoverflow.com/questions/26717277/accessing-a-json-object-in-bash-associative-array-list-another-model
 
 debug=false
 
+# https://stackoverflow.com/questions/2683279/how-to-detect-if-a-script-is-being-sourced
 sourced=0
 if [ -n "$ZSH_VERSION" ]; then 
   case $ZSH_EVAL_CONTEXT in *:file) sourced=1;; esac
@@ -22,6 +29,7 @@ debug && echo "final answer is name $name path $path"
 
 
 vartype() {
+    # https://stackoverflow.com/questions/14525296/how-do-i-check-if-variable-is-an-array
     local var=$( declare -p $1 )
     local reg='^declare -n [^=]+=\"([^\"]+)\"$'
     while [[ $var =~ $reg ]]; do
@@ -57,6 +65,7 @@ debug && echo "all script args ${@}"
 debug && echo "all incoming parameter values ${args[@]}"
 debug && echo "accepted agruments ${!ARGUMENTS[@]}"
 debug && vartype $ARGUMENTS
+# https://unix.stackexchange.com/questions/366581/bash-associative-array-printing
 debug && printf "%s\n" "${!ARGUMENTS[@]}" "${ARGUMENTS[@]}" | pr -2t
 
 #if vartype is string then
@@ -100,6 +109,7 @@ while [[ $# -gt 0 ]]; do
      shift 2
      continue
   fi
+  # https://stackoverflow.com/questions/13219634/easiest-way-to-check-for-an-index-or-a-key-in-an-array
   if [ "--${ARGUMENTS[${1}]+abc}" ]; then
     declare -x -g ${1:2}=$2
     export ${1:2}=$2
