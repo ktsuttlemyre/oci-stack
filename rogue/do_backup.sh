c236775a6931bc75e8b5bb7d317349757192b773
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2022-12-11 20:35:41 -0500

    Update do_backup.sh

--- rogue/do_backup.sh
+++ rogue/do_backup.sh
@@ -2,6 +2,46 @@
 #expect root to run this in home directory
 # https://help.ubuntu.com/community/BackupYourSystem/TAR
 # $1 = BACKUPNAME
+  GNU nano 5.4                                                   test.sh *                                                          
+#!/bin/bash
+
+
+declare -A ARGUMENTS=( [name]=system_root [path]=/ )
+
+
+#VARS="`set -o posix ; set`";
+#      arguments      #
+
+#ARGUMENTS="`grep -vFe "$VARS" <<<"$(set -o posix ; set)" | grep -v ^VARS= | cut -d "=" -f 1 | tr "\n" " "`"
+#ARGUMENTS=($ARGUMENTS)
+
+echo "incoming parameters ${!ARGUMENTS[@]}"
+
+# read arguments
+opts=$(getopt \
+  --longoptions "$(printf "%s:," "${!ARGUMENTS[@]}")" \
+  --name "$(basename "$0")" \
+  --options "" \
+  -- "$@"
+)
+eval set --$opts
+
+while [[ $# -gt 0 ]]; do
+  echo "looking at $1 with $2 arguments ${!ARGUMENTS[@]}"
+  if [ "--${ARGUMENTS[${1}]+abc}" ]; then
+    declare -x -g ${1:2}=$2
+    echo "set ${1:2} as $2"
+  fi
+  shift 3
+done
+
+
+#declare -A ARGUMENTS=( [name]=system_root [path]=/ )
+#kwargs $@ args
+
+
+echo "name $name path $path"
+
 
 #make space
 #ls -1tr | head -n -10 | xargs -d '\n' rm -f --
@@ -12,5 +52,5 @@
 #see --one-file-system argument here
 #https://help.ubuntu.com/community/BackupYourSystem/TAR
 #excludes=$(/proc/mounts | cut -d" " -f 2 | awk '{print " --exclude=" $0}' | tr -d "\n")
-bup index --exclude="/root/backups/" --one-file-system --exclude="/root/.bup" /
-bup save --name system_root /
+bup index --exclude="/root/backups/" --one-file-system --exclude="/root/.bup" $path
+bup save --name "$name" $path
