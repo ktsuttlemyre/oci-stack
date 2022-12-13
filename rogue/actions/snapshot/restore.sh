d55e7c0d2bf658e0192ad6f2a1b0e58d4a4ff0bd
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2022-12-13 15:36:21 -0500

    Update restore.sh

--- rogue/actions/snapshot/restore.sh
+++ rogue/actions/snapshot/restore.sh
@@ -3,7 +3,7 @@ unpack_to_ram=false
 exclude_mounts=$(cat /proc/mounts | cut -d" " -f 2 | awk '{print " --exclude=" $0}' | tr -d "\n")
 exclude_systems="--exclude=/dev --exclude=/proc --exclude=/sys --exclude=/tmp/ --exclude=/run/ --exclude=/mnt/ --exclude=/media/ --exclude=/lost+found"
 
-tmp_file=$(mktemp -d -t snapshot_restoreXXXXXXXXX)
+tmp_file=$(mktemp -d -t snapshot-restore-XXXXXXXXX)
 
 if [ unpack_to_ram ]; then
   mount -t tmpfs -o size=$(awk '/MemFree/ { printf "%.3f \n", $2/1024/1024 }' /proc/meminfo)G restore $tmp_file
