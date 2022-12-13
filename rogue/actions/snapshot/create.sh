86ac94a6bc06bcaee5323de2b3aded9f7c285714
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2022-12-13 15:18:02 -0500

    Update create.sh

--- rogue/actions/snapshot/create.sh
+++ rogue/actions/snapshot/create.sh
@@ -53,4 +53,4 @@ echo "name $name path $path"
 exclude_mounts=$(cat /proc/mounts | cut -d" " -f 2 | awk '{print " --exclude=" $0}' | tr -d "\n")
 exclude_systems="--exclude=/dev --exclude=/proc --exclude=/sys --exclude=/tmp/ --exclude=/run/ --exclude=/mnt/ --exclude=/media/ --exclude=/lost+found"
 bup index --one-file-system --exclude="/root/.bup" $exclude_mounts $exclude_systems $path
-bup save --name "$name" $path
+bup save --quiet --name "$name" $path
