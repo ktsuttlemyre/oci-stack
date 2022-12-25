d47ff30826312f8a2174e2dcef02057ad434a947
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2022-12-25 16:43:13 -0500

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -5,6 +5,25 @@ locals {
     tcp    = 6
     udp    = 17
   }
+  
+ inbound_tcp = [
+     { port = 80, description = "http (should always redirect from trafik)" }, #http
+     { port = 443, description = "https" }, #https
+  ]
+  inbound_udp = [
+      { port = 443, description = "https" }, #https
+      { port = 6000, description = "restreamer srt" }, #streaming
+  ]
+#  { port = 53, protocol = “-1” },    # DNS
+#   { port = 88, protocol = “-1” },    # Kerberos
+#   { port = 123, protocol = “udp” },  # Time Sync (NTP)
+#   { port = 135, protocol = “tcp” },  # RPC Endpoint Mapper
+#   { port = 389, protocol = “-1” },   # LDAP
+#   { port = 445, protocol = “tcp” },  # SMB
+#   { port = 464, protocol = “-1” },   # Kerberos (password)
+#   { port = 636, protocol = “tcp” },  # LDAP SSL
+#   { port = 3268, protocol = “tcp” }, # LDAP Global Catalog
+#   { port = 3269, protocol = “tcp” }  # LDAP Global Catalog SSL
 
   shapes = {
     flex : "VM.Standard.A1.Flex",
@@ -19,3 +38,4 @@ locals {
     ]
   )
 }
+
